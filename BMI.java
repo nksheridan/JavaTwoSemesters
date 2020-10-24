e246fda5615ecd07791174fe6d51af41fdef45b2
Neil K. Sheridan <nk.sheridan@gmail.com>
2020-10-24 16:23:06 +0100

    Create BMI.java

---
+++ BMI.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//package to help with keyboard input
+
+public class FindCost {
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		//create object keyboard of scanner class (system.in) represents keyboard in java
+		
+		
+		System.out.println ("*** BMI Calculator***");
+		System.out.println ("Enter your weight: ");
+		double weight = keyboard.nextDouble(); //input method
+		System.out.println ("Enter your height: ");
+		double height = keyboard.nextDouble(); //input method
+		double BMI = weight / (height * height); //calculate using variables
+		System.out.println ("BMI = " + BMI);
+		
+
+	}
+
+}
